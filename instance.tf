de476a91176bcd61dd0e423c1b5335e06abdaebe
subhadeepdas-9831 <68052252+subhadeepdas-9831@users.noreply.github.com>
2023-11-22 20:15:03 +0530

    instance.tf

--- instance.tf
+++ instance.tf
@@ -1,1 +1,11 @@
 provider "aws" {
+    access_key = "ACCESS KEY"
+    secret_key = "SECRET KEY"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "testinstance" {
+    ami = ""
+    instance_type = "t2.micro"
+  
+}
